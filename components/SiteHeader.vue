b0769fbe3123c353a226e8951975f8adb683a4aa
jafar <beethove@lvb.com>
2019-02-11 15:07:18 +0000

    Modified SiteHeader.vue - Added CSS classes

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,13 +1,14 @@
 <template>
-  <header>
-    <h1>
+  <header class="SiteHeader">
+    <h1 class="SiteHeader__title">
       BBC Weather
     </h1>
-    <nav>
+    <nav class="SiteHeaderNav">
       <ul>
         <li
           v-for=" city in cities "
           :key=" city.index "
+          class="SiteHeaderNav__item"
         >
           <a @click.prevent=" emitCityClicked(city) ">
             {{ city.name }}
