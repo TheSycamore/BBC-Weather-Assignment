87148ff006956e711a0592089ac4eb5a6bff3bfa
jafar <beethove@lvb.com>
2019-02-11 12:17:42 +0000

    Created API files related to cities. Contain city: name, slug, coordinates

---
+++ server/citiesAvailable.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'London',
+    slug: 'london'
+  },
+  {
+    name: 'Paris',
+    slug: 'paris'
+  },
+  {
+    name: 'New York',
+    slug: 'new-york'
+  },
+  {
+    name: 'Singapore',
+    slug: 'singapore'
+  },
+  {
+    name: 'Sydney',
+    slug: 'sydney'
+  }
+]
---
+++ server/cityLocations.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    city: 'london',
+    coordinates: '51.5074,0.1278'
+  },
+  {
+    city: 'paris',
+    coordinates: '48.8566,2.3522'
+  },
+  {
+    city: 'new-york',
+    coordinates: '40.73061,-73.935242'
+  },
+  {
+    city: 'singapore',
+    coordinates: '1.3521,103.8198'
+  },
+  {
+    city: 'sydney',
+    coordinates: '-33.865143,151.2099'
+  }
+]
